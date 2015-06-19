adb7ad98f3a934c1f8e8db0d70c85f2424030815
Vladimir Utenkov <utenkov.vv87@gmail.com>
2015-06-19 19:04:37 +0300

    Fix typo

--- settings.py
+++ settings.py
@@ -1,2 +1,2 @@
-# Put your device token here. To get an device token, register at http://stage.cloud4rpi.io
+# Put your device token here. To get a device token, register at http://stage.cloud4rpi.io
 DeviceToken = ""
