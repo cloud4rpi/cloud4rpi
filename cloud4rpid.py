f868a4cce72c711a44a5bfb4d4e80d21242d01b1
Vladimir Utenkov <utenkov.vv87@gmail.com>
2015-07-20 19:06:03 +0300

    Skip failed streams

--- cloud4rpid.py
+++ cloud4rpid.py
@@ -9,6 +9,7 @@ import datetime
 import logging
 import subprocess
 
+from requests import RequestException
 from settings import DeviceToken
 
 import settings_vendor as config
@@ -245,7 +246,10 @@ class RpiDaemon:
 
     def send_stream(self):
         stream = self.create_stream()
-        post_stream(self.token, stream)
+        try:
+            post_stream(self.token, stream)
+        except RequestException:
+            pass
 
     def create_stream(self):
         ts = datetime.datetime.now().isoformat()
