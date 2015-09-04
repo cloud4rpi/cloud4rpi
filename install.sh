cc84d63e075e6e9d03316d1e16e6273352a383e0
Sky3d <sky3ddd@gmail.com>
2015-09-04 17:55:57 +0300

    install script - run as service

--- install.sh
+++ install.sh
@@ -1,1 +1,12 @@
 #!/bin/sh
+
+SERVICE_NAME=cloud4rpid.sh
+SOURCE_DIR=$HOME/cloud4rpi
+DEST_DIR=/etc/init.d
+
+sudo chmod 755 $SOURCE_DIR/cloud4rpid.py
+
+sudo cp $SOURCE_DIR/$SERVICE_NAME $DEST_DIR/$SERVICE_NAME
+sudo chmod 755 $DEST_DIR/$SERVICE_NAME
+sudo update-rc.d $SERVICE_NAME defaults
+
