dca22a33fed3a4d5f327e9812a99714d1dfc4434
Sky3d <sky3ddd@gmail.com>
2015-09-08 11:36:04 +0300

    install - checking unix new line strings

--- install.sh
+++ install.sh
@@ -3,19 +3,19 @@
 BASEDIR=$(dirname $0)
 SERVICE_NAME=cloud4rpid.sh
 
-echo "Prepare Init-script in ${BASEDIR}"
+echo "Preparing Init-script from ${BASEDIR}"
 python $BASEDIR/init_script_patcher.py
 
 echo "Copying Init-script into /etc/init.d"
 sudo cp $BASEDIR/$SERVICE_NAME /etc/init.d/$SERVICE_NAME
 
-echo "Copying Init-script into /etc/init.d"
-sudo cp $BASEDIR/$SERVICE_NAME /etc/init.d/$SERVICE_NAME
-
-
 echo "Set scripts permissions"
 sudo chmod 755 /etc/init.d/$SERVICE_NAME
 sudo chmod 755 $BASEDIR/cloud4rpid.py
+
+echo "Validating unix newline strings"
+sudo apt-get install dos2unix
+sudo dos2unix $BASEDIR/cloud4rpid.py
 
 echo "Updating rc.d"
 sudo update-rc.d $SERVICE_NAME defaults
