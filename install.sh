764a3241acf26fe6eb42ebbbfc60e99d15ba5631
Sky3d <sky3ddd@gmail.com>
2015-09-16 09:29:31 +0300

    remove sudo from installation script commands

--- install.sh
+++ install.sh
@@ -9,14 +9,14 @@ echo "Preparing Init-script from ${BASEDIR}"
 python $BASEDIR/init_script_gen.py
 
 echo "Copying Init-script to /etc/init.d"
-sudo cp $BASEDIR/$SERVICE_NAME /etc/init.d/$SERVICE_NAME
+cp $BASEDIR/$SERVICE_NAME /etc/init.d/$SERVICE_NAME
 
 echo "Set permissions of files"
-sudo chmod 755 /etc/init.d/$SERVICE_NAME
-sudo chmod 755 $BASEDIR/cloud4rpid.py
+chmod 755 /etc/init.d/$SERVICE_NAME
+chmod 755 $BASEDIR/cloud4rpid.py
 
 echo "Updating rc.d"
-sudo update-rc.d $SERVICE_NAME defaults
+update-rc.d $SERVICE_NAME defaults
 
 echo "Cloud4RPI service successfully installed!"
 echo -e "Please see the \e[1mcloud4rpid.log \e[0mfile in the /var/log/ directory to get logging information."
