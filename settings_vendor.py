bb5662c771157157ded8bd057313b07df7782418
Alexander Bulychev <alex@bulychev.net>
2015-10-13 16:50:09 +0300

    pylint fix 2

--- settings_vendor.py
+++ settings_vendor.py
@@ -1,5 +1,1 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-# Server parameters
-baseApiUrl = 'http://stage.cloud4rpi.io:3000/api'
+#!/usr/bin/env python# -*- coding: utf-8 -*-# Server parametersbaseApiUrl = 'http://stage.cloud4rpi.io:3000/api'
