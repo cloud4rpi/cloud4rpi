49199d284dee632ff81da47e1edd3b72f1624678
Sky3d <sky3ddd@gmail.com>
2015-12-25 16:59:32 +0300

    remove settings.py

--- c4r/__main__.py
+++ c4r/__main__.py
@@ -5,13 +5,7 @@ import os
 import time
 from subprocess import CalledProcessError
 import requests
-# from requests import RequestException
-#
-# import c4r.errors as errors
-# from c4r import lib
-from settings import DeviceToken
-from c4r.logger import get_logger #, config_logging_to_file
-# from settings import LOG_FILE_PATH
+from c4r.logger import get_logger
 
 
 log = get_logger()
@@ -31,7 +25,6 @@ def safe_run_daemon(lib):
     while n < max_try_count:
         try:
             lib.set_device_token(DeviceToken)
-            # lib.run()
             break
         except requests.ConnectionError as ex:
             log.exception('Daemon running ERROR: {0}'.format(ex.message))
@@ -40,42 +33,3 @@ def safe_run_daemon(lib):
             n += 1
             wait_secs *= 2
 
-# def main():
-#     daemon = None
-#     try:
-#         modprobe('w1-gpio')
-#         modprobe('w1-therm')
-#
-#         config_logging_to_file(log, LOG_FILE_PATH)
-#
-#         log.info('Starting...')
-#
-#         safe_run_daemon(lib)
-#
-#     except RequestException as e:
-#         log.exception('Connection failed. Please try again later. Error: {0}'.format(e.message))
-#         exit(1)
-#     except CalledProcessError:
-#         log.exception('Try "sudo python cloud4rpi.py"')
-#         exit(1)
-#     except errors.InvalidTokenError:
-#         log.exception('Device Access Token {0} is incorrect. Please verify it.'.format(DeviceToken))
-#         exit(1)
-#     except errors.AuthenticationError:
-#         log.exception('Authentication failed. Check your device token.')
-#         exit(1)
-#     except errors.NoSensorsError:
-#         log.exception('No sensors found... Exiting')
-#         exit(1)
-#     except Exception as e:
-#         log.exception('Unexpected error: {0}'.format(e.message))
-#         exit(1)
-#     except KeyboardInterrupt:
-#         log.info('Interrupted')
-#         if daemon:
-#             daemon.shutdown()
-#         exit(1)
-#
-#
-# if __name__ == "__main__":
-#     main()
