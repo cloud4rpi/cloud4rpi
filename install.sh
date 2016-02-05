0398cda07091640fc5ae98e07dab51e03343437b
Vladimir Utenkov <utenkov.vv87@gmail.com>
2016-02-05 11:03:26 +0300

    Remove init_script_gen.py

--- install.sh
+++ install.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 
-BASEDIR=$(dirname $0)
+function quit_on_error() {
+    test "0" = $? || {
+        exit 1
+    }
+}
+
 SERVICE_NAME=cloud4rpi
 
-echo "Installing cloud4rpi service..."
+echo "Generating init script..."
+cat ./cloud4rpi.tmpl | sed "s;%CLOUD4RPI_DIR%;$(pwd);" > $SERVICE_NAME
+echo "Done"
 
-mkdir tmp
+echo "Setting permissions..."
+chmod +x ./cloud4rpi
+echo "Done"
 
-echo "Preparing Init-script from ${BASEDIR}"
-python $BASEDIR/tools/init_script_gen.py
+echo "Copying init script to /etc/init.d"
+cp $SERVICE_NAME /etc/init.d/$SERVICE_NAME
+quit_on_error
+echo "Done"
 
-echo "Copying Init-script to /etc/init.d"
-cp $BASEDIR/tmp/$SERVICE_NAME /etc/init.d/$SERVICE_NAME
-
-rm -rf tmp
-
-echo "Set permissions of files"
-chmod 755 /etc/init.d/$SERVICE_NAME
-chmod 755 $BASEDIR/app.py
-chmod 755 $BASEDIR/c4r/*.py
-
-echo "Updating rc.d"
+echo "Installing init script links..."
 update-rc.d $SERVICE_NAME defaults
+quit_on_error
+echo "Done"
 
-echo "Cloud4RPI service successfully installed!"
 echo -e "Please see the \e[1mcloud4rpid.log \e[0mfile in the /var/log/ directory to get logging information."
 echo "Usage example:"
 echo -e "$ sudo service \e[1mcloud4rpi \e[0mstart|stop|status"
