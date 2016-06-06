bf273ed8899d5e1ee2b758e431b41eca32d11152
Alexander Bulychev <alex@bulychev.net>
2016-06-06 16:03:02 +0300

    mqtt connection recovery

--- c4r/mqtt_listener.py
+++ c4r/mqtt_listener.py
@@ -14,14 +14,16 @@ def raise_event(content):
 class MqttListener(object):
     def __init__(self):
         self.process = None
+        self.api_key = None
         self.client = mqtt.Client()
         self.client.on_message = self.on_message
-
+        self.client.on_connect = self.on_connect
+        self.client.on_disconnect = self.on_disconnect
 
     def start(self, api_key):
+        self.api_key = api_key
         log.info('MqttListener - starting')
         self.connect()
-        self.listen(api_key)
         self.client.loop_start()
         log.info('MqttListener - listening started')
 
@@ -39,11 +41,19 @@ class MqttListener(object):
             log.error('Connection failed: {0}'.format(e))
             raise e
 
-    def listen(self, api_key):
-        topic = helpers.format_subscription_topic(api_key)
-
+    def listen(self):
+        topic = helpers.format_subscription_topic(self.api_key)
         log.info('subscribing for [{0}]'.format(topic))
         self.client.subscribe(topic, 0)
+
+    def on_connect(self, client, userdata, flags, rc):
+        if rc != 0:
+            log.error('Connection failed: {0}'.format(rc))
+            raise Exception('Mqtt connection failed')
+        self.listen()
+
+    def on_disconnect(self, client, userdata, rc):
+        log.info('MqttListener disconnected. Reason: {0}. Reconnecting ...'.format(rc))
 
     def on_message(self, client, userdata, message):
         log.info('MQTT message received: [{0}] - [{1}]'.format(message.topic, message.payload))
