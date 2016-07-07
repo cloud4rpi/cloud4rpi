71679275bad7645741b03ae4fe3e7e9d8d5706e0
Alexander Bulychev <alex@bulychev.net>
2016-07-07 14:23:51 +0300

    tests fix

--- c4r/error_messages.py
+++ c4r/error_messages.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from requests import RequestException
 from subprocess import CalledProcessError
 from c4r import errors as c4r_errors
 
 messages = {
     KeyboardInterrupt: 'Interrupted',
-    RequestException: 'Connection failed. Please try again later. Error: {0}',
     CalledProcessError: 'Try "sudo python cloud4rpi.py"',
     c4r_errors.InvalidTokenError: 'API key {0} is incorrect. Please verify it.',
     c4r_errors.AuthenticationError: 'Authentication failed. Check your API key.',
