d21575207df77a61ef19423177b756e35a9ddf6f
VladG <vladislav.glagolev@devexpress.com>
2016-10-20 19:48:57 +0000

    supported CPU temperature on CHIP

--- c4r/cpu_temperature.py
+++ c4r/cpu_temperature.py
@@ -4,14 +4,22 @@
 import subprocess
 
 SUPPORTED_TYPE = 'cpu'
-CPU_TEMPERATURE_CMD = "vcgencmd measure_temp"
+RPI_CPU_TEMPERATURE_CMD = "vcgencmd measure_temp"
+CHIP_CPU_TEMPERATURE_CMD = "echo $(sudo i2cget -y -f 0 0x34 0x5e) $(sudo i2cget -y -f 0 0x34 0x5f)"
 
+PLATFORM = "RPI"
+if subprocess.check_output("uname -a", shell=True).find('Linux chip') == 0:
+    PLATFORM = "CHIP"
 
 class CpuTemperature(object):
     def __init__(self):
         self.type = SUPPORTED_TYPE
 
     def read(self):
-        cpu_temperature_str = subprocess.check_output(CPU_TEMPERATURE_CMD, shell=True) \
-            .lstrip("temp=").rstrip("'C\n")
-        return float(cpu_temperature_str)
+	if PLATFORM == "CHIP":
+            msb, lsb = subprocess.check_output(CHIP_CPU_TEMPERATURE_CMD, shell=True).split()
+            cpu_temperature = (int(msb, 0) << 4 | int(lsb, 0) & 15) / 10 - 144.7
+        elif PLATFORM == "RPI":
+            cpu_temperature = float(subprocess.check_output(RPI_CPU_TEMPERATURE_CMD, shell=True) \
+                .lstrip("temp=").rstrip("'C\n"))
+        return cpu_temperature
