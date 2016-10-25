8b46b64246c3675b25332b4650496a2a4a12b8a1
Vladimir Utenkov <utenkov.vv87@gmail.com>
2016-10-25 09:45:26 +0300

    Shorten a line

--- c4r/cpu_temperature.py
+++ c4r/cpu_temperature.py
@@ -3,15 +3,12 @@
 
 import subprocess
 
-SUPPORTED_TYPE = 'cpu'
 CPU_TEMPERATURE_CMD = "vcgencmd measure_temp"
 
 
 class CpuTemperature(object):
-    def __init__(self):
-        self.type = SUPPORTED_TYPE
-
     def read(self):
-        cpu_temperature_str = subprocess.check_output(CPU_TEMPERATURE_CMD, shell=True) \
-            .lstrip("temp=").rstrip("'C\n")
-        return float(cpu_temperature_str)
+        temp_str = subprocess.check_output(CPU_TEMPERATURE_CMD, shell=True) \
+            .lstrip("temp=") \
+            .rstrip("'C\n")
+        return float(temp_str)
