29e3af79eb40b10ef91c4dc547b02609ec3e206a
Sky3d <sky3ddd@gmail.com>
2016-11-28 13:01:03 +0300

    increase version to 0.0.9

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except Exception:
     pass
 
 setup(name='cloud4rpi',
-      version='0.0.8',
+      version='0.0.9',
       description='cloud4rpi client library',
       long_description=description,
       url='https://github.com/cloud4rpi/cloud4rpi',
