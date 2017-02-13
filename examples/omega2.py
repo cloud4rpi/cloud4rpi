1804eb1c3689aa965cda112da0b4e9f621b9ace8
Himura Kazuto <Himura2la@users.noreply.github.com>
2017-02-13 13:11:17 +0300

    pep8[2]

--- examples/omega2.py
+++ examples/omega2.py
@@ -31,7 +31,7 @@ for _, pin in RGB.items():
 def RGB_control(led, value):
     operation = 'clear' if value else 'set'  # (sic)
     try:
-        return not subprocess.call("gpioctl %s %s" % (operation, RGB[led]), 
+        return not subprocess.call("gpioctl %s %s" % (operation, RGB[led]),
                                    shell=True)
     except KeyError:
         return False
